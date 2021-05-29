646dd2d7b10a11644b84cce10d23e538e9fad2cb
lucien-macbook-15 <lucienduan@tencent.com>
2021-05-29 19:09:44 +0800

    feat: decotator

--- src/decotator.ts
+++ src/decotator.ts
@@ -1,29 +1,38 @@
 
-//暂没用到
 export  function hook(method:string = 'emit'): any { 
-  return function(plug, name: string, descriptor) {
-    const suffix = upperFirstByte(name) 
+  return function(plug, methodName: string, descriptor) {
+    const suffix = upperFirstByte(methodName) 
     const fn = descriptor.value
     descriptor.value = async function(...rest) {
-      console.log('hook', name, plug[method])
+      console.log('hook', methodName, plug[method])
       plug[method]('before' + suffix, rest)
-      let result = await fn.apply(this, rest)
-      plug[method]('after' + suffix, result, rest)
-      return result 
+      try {
+        let result = await fn.apply(this, rest)
+        plug[method](methodName + 'Success', result, rest)
+        return result 
+      } catch (error) {
+        plug[method](methodName + 'Failed', error, rest)
+        throw error
+      }
     }
   }
 }
 
 export  function hookSync(method:string = 'emitAsyncParalle'): any { 
-  return function(plug, name: string, descriptor) {
-    const suffix = upperFirstByte(name) 
+  return function(plug, methodName: string, descriptor) {
+    const suffix = upperFirstByte(methodName) 
     const fn = descriptor.value
     descriptor.value = async function(...rest) {
       console.log('hookSync', method, plug)
       await plug[method]('before' + suffix, rest)
-      let result = await fn.apply(this, rest)
-      await plug[method]('after' + suffix, result, rest)
-      return result 
+      try {
+        let result = await fn.apply(this, rest)
+        await plug[method](methodName + 'Success', result, rest)
+        return result 
+      } catch (error) {
+        await plug[method](methodName + 'Failed', error, rest)
+        throw error
+      }
     }
   }
 }
